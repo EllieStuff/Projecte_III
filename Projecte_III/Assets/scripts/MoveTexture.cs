c73cbe8d923358763265945c45dbd722901114b4
xavi <30593493+xdman21@users.noreply.github.com>
2022-02-24 17:06:41 +0100

    more code optimization

--- Projecte_III/Assets/scripts/MoveTexture.cs
+++ Projecte_III/Assets/scripts/MoveTexture.cs
@@ -8,16 +8,17 @@ public class MoveTexture : MonoBehaviour
 
     public float scrollSpeedX = 0.5f;
     public float scrollSpeedY = 0.5f;
+    private Renderer render;
 
     void Start()
     {
-
+        render = GetComponent<Renderer>();
     }
 
     void Update()
     {
         float offsetX = Time.time * scrollSpeedX;
         float offsetY = Time.time * scrollSpeedY;
-        this.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+        render.material.mainTextureOffset = new Vector2(offsetX, offsetY);
     }
 }
