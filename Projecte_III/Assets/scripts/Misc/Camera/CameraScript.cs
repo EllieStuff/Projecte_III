280c830ac6933bdd10b670cf7e12a921e2d707a1
Joel <joel.mateu@enti.cat>
2022-02-26 19:47:04 +0100

    algo mas de orden

--- Projecte_III/Assets/scripts/Misc/Camera/CameraScript.cs
+++ Projecte_III/Assets/scripts/Misc/Camera/CameraScript.cs
@@ -41,25 +41,7 @@ public class CameraScript : MonoBehaviour
         //transform.rotation = Quaternion.Euler(rotOffset);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotOffsetQuat, Time.deltaTime * camRotSpeed);
         //transform.rotation = rotOffsetQuat;
-
-
-        //if (controls.Quad.LookBackwards.ReadValue<float>() > 0/*Input.GetKeyDown(KeyCode.Tab)*/)
-        //    this.transform.rotation = rotOffsetQuat * lookBackRotOffset;
-        //else /*if (Input.GetKeyUp(KeyCode.Tab))*/
-        //    this.transform.rotation = rotOffsetQuat;
-
-
-        // Test Mode
-        //if (Input.GetKey(KeyCode.Tab))
-        //    this.transform.rotation = Quaternion.Euler(rotOffset) * lookBackRotOffset;
-        //else
-        //    this.transform.rotation = Quaternion.Euler(rotOffset);
-
-
-        //if (Input.GetKey(KeyCode.Tab))
-        //    this.transform.rotation = Quaternion.Lerp(this.transform.rotation, new Quaternion(this.transform.rotation.x, playerVehicle.transform.rotation.y, this.transform.rotation.z, playerVehicle.transform.rotation.w) * Quaternion.Euler(new Vector3(0, 180, 0)), Time.deltaTime * camSpeed);
-        //else
-        //    this.transform.rotation = Quaternion.Lerp(this.transform.rotation, new Quaternion(this.transform.rotation.x, playerVehicle.transform.rotation.y, this.transform.rotation.z, playerVehicle.transform.rotation.w), Time.deltaTime * camSpeed);
+      
     }
 
 
