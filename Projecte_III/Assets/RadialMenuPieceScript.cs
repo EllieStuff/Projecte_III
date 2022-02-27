043861a27832887da7f0d044947f08a5ca2e682c
Joel <joel.mateu@enti.cat>
2022-02-27 16:48:48 +0100

    CreatingRadialMenu

---
+++ Projecte_III/Assets/RadialMenuPieceScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMenuPieceScript : MonoBehaviour
+{
+    public Image backGround;
+    public Image icon;
+    
+    Color targetBgColor;
+    float lerpBgColorTime = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetBgColor = backGround.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LerpBgColor(Color _color)
+    {
+        if(_color != targetBgColor)
+        {
+            targetBgColor = _color;
+            StartCoroutine(LerpBgColorCoroutine(_color));
+        }
+    }
+
+    IEnumerator LerpBgColorCoroutine(Color _color)
+    {
+        float timer = 0;
+        Color initColor = backGround.color;
+        while(timer < lerpBgColorTime)
+        {
+            backGround.color = Color.Lerp(initColor, _color, timer / lerpBgColorTime);
+
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+        backGround.color = _color;
+
+    }
+
+}
