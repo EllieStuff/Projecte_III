b80385b3a2b435cf541bfedf70868c343fb66ae1
Milion16 <joel.mateu@enti.cat>
2022-03-09 16:16:50 +0100

    Camera stuff optimized with list and dict

--- Projecte_III/Assets/Scripts/Misc/Camera/ClearObstaclesCameraRaycast.cs
+++ Projecte_III/Assets/Scripts/Misc/Camera/ClearObstaclesCameraRaycast.cs
@@ -13,13 +13,15 @@ public class ClearObstaclesCameraRaycast : MonoBehaviour
     Transform player;
 
     //Transform player, camera;
+    public enum ObstacleState { DEFAULT, APPEARING, DISAPPEARING }
     class ObstacleData
     {
-        public MeshRenderer renderer; public Material savedMaterial;
+        public MeshRenderer renderer; public Material savedMaterial; public ObstacleState state = ObstacleState.DISAPPEARING;
         public ObstacleData(MeshRenderer _renderer, Material _savedMaterial) { renderer = _renderer; savedMaterial = _savedMaterial; }
     }
-    List<ObstacleData> obstaclesData = new List<ObstacleData>();
-    //List<MeshRenderer> obstaclesMeshes = new List<MeshRenderer>();
+    List<ObstacleData> obstaclesDataList = new List<ObstacleData>();
+    Dictionary<Transform, ObstacleData> obstaclesDataDict = new Dictionary<Transform, ObstacleData>();
+    List<MeshRenderer> obstaclesMeshes = new List<MeshRenderer>();
 
     // Start is called before the first frame update
     void Start()
@@ -30,35 +32,31 @@ public class ClearObstaclesCameraRaycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 currMargin = player.forward * forwardMargin; 
-        //if (player.Backward)
-        //    currMargin = Vector3.zero;
+        Vector3 currMargin = player.forward * forwardMargin;
         currMargin.y += downMargin;
         Vector3 cameraPos = Camera.main.transform.position + currMargin;
         Vector3 playerPos = player.position + currMargin;
 
         Debug.DrawLine(cameraPos, playerPos, Color.red);
-        //RaycastHit[] hits = Physics.SphereCastAll(new Ray(player.position, player.up), clearingRadius, 20.0f);
-        //UpdateHits(hits);
         RaycastHit[] hits = Physics.SphereCastAll(new Ray(playerPos, cameraPos - playerPos), clearingRadius, Vector3.Distance(cameraPos, playerPos));
-        UpdateHits(hits);
-
+        //UpdateHitsList(hits);
+        UpdateHitsDictionary(hits);
     }
 
-    void UpdateHits(RaycastHit[] _hits)
+    void UpdateHitsList(RaycastHit[] _hits)
     {
-        if (_hits.Length > 0)
+        // Erase Unnecesari Data
+        if (obstaclesDataList.Count > 0)
         {
-            // Erase Unnecesari Data
             int idx = 0;
-            while (idx < obstaclesData.Count)
+            while (idx < obstaclesDataList.Count)
             {
                 bool found = false;
                 for (int i = 0; i < _hits.Length && !found; i++)
                 {
                     if (_hits[i].transform.tag == "Tree")
                     {
-                        if (obstaclesData[idx].renderer.transform == _hits[i].transform)
+                        if (obstaclesDataList[idx].renderer.transform == _hits[i].transform)
                             found = true;
                     }
                 }
@@ -66,38 +64,91 @@ public class ClearObstaclesCameraRaycast : MonoBehaviour
                 if (found) idx++;
                 else
                 {
-                    StartCoroutine(ReappearObstacle(obstaclesData[idx]));
-                    obstaclesData.RemoveAt(idx);
+                    StartCoroutine(ReappearObstacle(obstaclesDataList[idx]));
+                    obstaclesDataList.RemoveAt(idx);
                 }
             }
+        }
 
-            // Looks For new Data
-            for (int i = 0; i < _hits.Length; i++)
+        // Looks For new Data
+        for (int i = 0; i < _hits.Length; i++)
+        {
+            //Debug.Log("Colliding " + _hits[i].transform.tag);
+            if (_hits[i].transform.tag == "Tree")
             {
-                //Debug.Log("Colliding " + _hits[i].transform.tag);
-                if (_hits[i].transform.tag == "Tree")
+                if (obstaclesDataList.Find(_obs => _obs.renderer.transform == _hits[i].transform) == null)
                 {
-                    if (obstaclesData.Find(_obs => _obs.renderer.transform == _hits[i].transform) == null)
+                    MeshRenderer renderer = _hits[i].transform.GetComponent<MeshRenderer>();
+                    if (renderer != null)
                     {
-                        MeshRenderer renderer = _hits[i].transform.GetComponent<MeshRenderer>();
-                        if (renderer != null)
-                        {
-                            ObstacleData newObstacleData = new ObstacleData(renderer, renderer.material);
-                            obstaclesData.Add(newObstacleData);
-                            StartCoroutine(ClearObstacle(newObstacleData));
-                        }
+                        ObstacleData newObstacleData = new ObstacleData(renderer, renderer.material);
+                        obstaclesDataList.Add(newObstacleData);
+                        StartCoroutine(ClearObstacle(newObstacleData));
                     }
                 }
             }
-
         }
+
+    }
+    
+    void UpdateHitsDictionary(RaycastHit[] _hits)
+    {
+        // Erase Unnecesari Data
+        if (obstaclesDataDict.Count > 0)
+        {
+            foreach (Transform key in obstaclesDataDict.Keys)
+            {
+                bool found = false;
+                for (int i = 0; i < _hits.Length && !found; i++)
+                {
+                    if (IsObstacle(_hits[i].transform.tag) && key == _hits[i].transform)
+                        found = true;
+                }
+
+                if (!found && obstaclesDataDict[key].state == ObstacleState.DEFAULT)
+                {
+                    obstaclesDataDict[key].state = ObstacleState.APPEARING;
+                    StartCoroutine(ReappearObstacle(obstaclesDataDict[key], key));
+                }
+
+            }
+        }
+
+        // Looks For new Data
+        for (int i = 0; i < _hits.Length; i++)
+        {
+            Transform currHit = _hits[i].transform;
+            if (IsObstacle(currHit.tag))
+            {
+                if (!obstaclesDataDict.ContainsKey(currHit))
+                {
+                    MeshRenderer renderer = currHit.GetComponent<MeshRenderer>();
+                    if (renderer != null)
+                    {
+                        ObstacleData newObstacleData = new ObstacleData(renderer, renderer.material);
+                        obstaclesDataDict.Add(currHit, newObstacleData);
+                        StartCoroutine(ClearObstacle(newObstacleData, currHit));
+                    }
+                }
+                else if(obstaclesDataDict[currHit].state == ObstacleState.APPEARING)
+                {
+                    obstaclesDataDict[currHit].state = ObstacleState.DISAPPEARING;
+                    StartCoroutine(ClearObstacle(obstaclesDataDict[currHit], currHit));
+                }
+            }
+        }
+    }
+
+    bool IsObstacle(string _objectTag)
+    {
+        return _objectTag == "Tree";
     }
 
 
     IEnumerator ReappearObstacle(ObstacleData _obsData)
     {
+        yield return new WaitForEndOfFrame();
         Material targetMaterial = _obsData.savedMaterial;
-        //prevMaterials.RemoveAt(0);
         Color targetColor = targetMaterial.color;
 
         float timer = 0.0f;
@@ -107,11 +158,31 @@ public class ClearObstaclesCameraRaycast : MonoBehaviour
             timer += Time.deltaTime;
             _obsData.renderer.material.color = Color.Lerp(_obsData.renderer.material.color, targetColor, timer / lerpTime);
         }
+        yield return new WaitForEndOfFrame();
         _obsData.renderer.material = targetMaterial;
+    }
+    IEnumerator ReappearObstacle(ObstacleData _obsData, Transform _key)
+    {
+        yield return new WaitForEndOfFrame();
+        Material targetMaterial = _obsData.savedMaterial;
+        Color targetColor = targetMaterial.color;
+
+        float timer = 0.0f;
+        while (timer < lerpTime)
+        {
+            yield return new WaitForEndOfFrame();
+            if (obstaclesDataDict[_key].state != ObstacleState.APPEARING)
+                yield break;
+            timer += Time.deltaTime;
+            _obsData.renderer.material.color = Color.Lerp(_obsData.renderer.material.color, targetColor, timer / lerpTime);
+        }
+        yield return new WaitForEndOfFrame();
+        _obsData.renderer.material = targetMaterial;
+        obstaclesDataDict.Remove(_key);
     }
     IEnumerator ClearObstacle(ObstacleData _obsData)
     {
-        //prevMaterials.Add(_obsMesh.material);
+        yield return new WaitForEndOfFrame();
         _obsData.renderer.material = new Material(auxPolyBrushMat);
         Color targetColor = _obsData.renderer.material.color;
         targetColor.a = 0.3f;
@@ -123,6 +194,22 @@ public class ClearObstaclesCameraRaycast : MonoBehaviour
             timer += Time.deltaTime;
             _obsData.renderer.material.color = Color.Lerp(_obsData.renderer.material.color, targetColor, timer / lerpTime);
         }
+    }
+    IEnumerator ClearObstacle(ObstacleData _obsData, Transform _key)
+    {
+        yield return new WaitForEndOfFrame();
+        _obsData.renderer.material = new Material(auxPolyBrushMat);
+        Color targetColor = _obsData.renderer.material.color;
+        targetColor.a = 0.3f;
+
+        float timer = 0.0f;
+        while (timer < lerpTime)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+            _obsData.renderer.material.color = Color.Lerp(_obsData.renderer.material.color, targetColor, timer / lerpTime);
+        }
+        obstaclesDataDict[_key].state = ObstacleState.DEFAULT;
     }
 
 
