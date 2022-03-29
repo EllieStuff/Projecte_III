a42943cd7e121833e0e4a132e6ae21876fd89ebf
xavifm <60983549+xavifm@users.noreply.github.com>
2022-03-29 14:39:30 +0200

    added more details to the test scene

--- Projecte_III/Assets/scripts/Misc/BoxModifierScript.cs
+++ Projecte_III/Assets/scripts/Misc/BoxModifierScript.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class BoxModifierScript : MonoBehaviour
 {
+    bool destroy;
+    float timer = 2;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag.Contains("Player")) 
         {
             collision.transform.GetComponent<RandomModifierGet>().GetModifier();
-            Destroy(gameObject);
+            destroy = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(destroy)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0 || transform.localScale.magnitude <= 0.2f)
+                Destroy(gameObject);
+            else
+                transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
         }
     }
 }
