e966fbab30157491449ce0ae3d40ba53d8d03b6b
Joel <joel.mateu@enti.cat>
2022-04-02 13:41:06 +0200

    Functioning

--- Projecte_III/Assets/scripts/Procedural Map/GenerateNewTile.cs
+++ Projecte_III/Assets/scripts/Procedural Map/GenerateNewTile.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GenerateNewTile : MonoBehaviour
 {
-    [SerializeField] GameObject[] tiles;
+    [SerializeField] RoadData[] tiles;
 
-    [SerializeField] Transform lastTile = null;
+    [SerializeField] RoadData lastTile = null;
+
+    RoadData.SpawnRateSet maxSpawnRates = new RoadData.SpawnRateSet(0);
+
+    List<RoadData> straightRoads = new List<RoadData>();
+    List<RoadData> leftRoads = new List<RoadData>();
+    List<RoadData> rightRoads = new List<RoadData>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        tiles = Resources.LoadAll<GameObject>("Prefabs/ProceduralMap");
-
+        GameObject[] tilesGO = Resources.LoadAll<GameObject>("Prefabs/ProceduralMap");
+        tiles = new RoadData[tilesGO.Length];
+        for (int i = 0; i < tilesGO.Length; i++)
+        {
+            tiles[i] = tilesGO[i].GetComponent<RoadData>();
+            if (tiles[i].RoadType == RoadData.Type.STRAIGHT)
+            {
+                straightRoads.Add(tiles[i]);
+                maxSpawnRates.straight += tiles[i].SpawnRate;
+            }
+            else if (tiles[i].RoadType == RoadData.Type.LEFT)
+            {
+                leftRoads.Add(tiles[i]);
+                maxSpawnRates.left += tiles[i].SpawnRate;
+            }
+            else if (tiles[i].RoadType == RoadData.Type.RIGHT)
+            {
+                rightRoads.Add(tiles[i]);
+                maxSpawnRates.right += tiles[i].SpawnRate;
+            }
+        }
        
     }
 
@@ -24,22 +52,17 @@ public class GenerateNewTile : MonoBehaviour
     public void CalculateNewTile()
     {
         float random = Random.Range(0, 100);
-
-        GameObject newObject = null;
-        if (random < 30)
-        { //Straight
-            newObject = Instantiate(tiles[0], transform);
-        }
-        else if( 30 < random && random < 60)
-        { //Turn right
-            newObject = Instantiate(tiles[1], transform);
-        }
-        else
-        { //Turn left
-            newObject = Instantiate(tiles[2], transform);
-        }
-
+        RoadData.Type roadType = lastTile.GetRoadType(random);
+        RoadData newObject = null;
+        if (roadType == RoadData.Type.STRAIGHT)
+            newObject = GetNewRoad(ref straightRoads, maxSpawnRates.straight);
+        else if (roadType == RoadData.Type.LEFT)
+            newObject = GetNewRoad(ref leftRoads, maxSpawnRates.left);
+        else if (roadType == RoadData.Type.RIGHT)
+            newObject = GetNewRoad(ref rightRoads, maxSpawnRates.right);
+        
         if (newObject == null) Debug.LogError("Upsie");
+
 
         Transform child = lastTile.transform.GetChild(0).Find("NewSpawn");
 
@@ -52,6 +75,26 @@ public class GenerateNewTile : MonoBehaviour
         if(transform.childCount > 2)
             Destroy(transform.GetChild(0).gameObject);
 
-        lastTile = newObject.transform;
+        lastTile = newObject;
     }
+
+
+    RoadData GetNewRoad(ref List<RoadData> _roadList, float _maxSpawnRate)
+    {
+        float random = Random.Range(0, _maxSpawnRate);
+        float currRndAmount = 0;
+        foreach (RoadData road in _roadList)
+        {
+            if (random > currRndAmount && random < currRndAmount + road.SpawnRate)
+            {
+                GameObject newRoad = Instantiate(road.gameObject, transform);
+                return newRoad.GetComponent<RoadData>();
+            }
+
+            currRndAmount += road.SpawnRate;
+        }
+
+        return null;
+    }
+
 }
