182cac00cf071db7e9d4c2bad7c5d38184977de0
xavi <30593493+xdman21@users.noreply.github.com>
2022-04-22 15:12:06 +0200

    more bugfixing added

--- Projecte_III/Assets/Scripts/Misc/RoundManager.cs
+++ Projecte_III/Assets/Scripts/Misc/RoundManager.cs
@@ -42,7 +42,7 @@ public class RoundManager : MonoBehaviour
             Transform _initPos = initPos.GetInitPos(i);
             player.transform.position = _initPos.position;
             player.transform.rotation = _initPos.rotation * Quaternion.Euler(0, 180, 0);
-            player.gameObject.SetActive(true);
+            player.transform.parent.gameObject.SetActive(true);
         }
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
