5a288c8bb7fb628f70f80defa5276134c20809ca
Joel <joel.mateu@enti.cat>
2022-04-30 02:32:30 +0200

    ButterflyEffect

---
+++ Projecte_III/Assets/ButterflyBehaviour.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyBehaviour : MonoBehaviour
+{
+    const float MARGIN = 1.0f;
+    const float CAMERA_MARGIN = 0.5f;
+
+    [SerializeField] Transform butterflyModel;
+    [SerializeField] Transform target;
+    [Space]
+    [SerializeField] Utils.MinMaxFloat butterflySpeedMult;
+    [SerializeField] float butterflyBaseSpeed;
+    [SerializeField] float butterflyBaseAngularSpeed;
+    [Space]
+    [SerializeField] Utils.MinMaxFloat changeTargetLocRate;
+    [SerializeField] Utils.MinMaxVec3 minMaxTargetLocMargin;
+
+    Rigidbody rb;
+    Transform camera;
+    Quaternion initModelRot;
+    float actualSpeed, actualAngularSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = butterflyModel.GetComponent<Rigidbody>();
+        camera = Camera.main.transform;
+        float speedMult = butterflySpeedMult.GetRndValue();
+        actualSpeed = butterflyBaseSpeed * speedMult;
+        actualAngularSpeed = butterflyBaseAngularSpeed * speedMult;
+
+        StartCoroutine(ChangeButterflyTargetLocation());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        butterflyModel.rotation = Quaternion.Lerp(butterflyModel.rotation, target.rotation, actualAngularSpeed * Time.deltaTime);
+        rb.MovePosition(butterflyModel.position + butterflyModel.forward * actualSpeed * Time.deltaTime);
+
+        RefreshTargetLocation();
+    }
+
+    void RefreshTargetLocation()
+    {
+        float modelToTarget_Dist = Vector3.Distance(butterflyModel.position, target.position);
+        if (modelToTarget_Dist < MARGIN || Physics.Raycast(butterflyModel.position, butterflyModel.forward, MARGIN)
+            || Vector3.Distance(butterflyModel.position, camera.position) < CAMERA_MARGIN)
+        {
+            do
+            {
+                SetNewTargetLocation();
+            } while (Physics.Raycast(butterflyModel.position, target.position - butterflyModel.position, modelToTarget_Dist));
+        }
+    }
+    void SetNewTargetLocation()
+    {
+        target.position = butterflyModel.position + minMaxTargetLocMargin.GetRndValue();
+        Vector3 targetMoveDir = (target.position - butterflyModel.position).normalized;
+        target.rotation = Quaternion.LookRotation(targetMoveDir, target.up);
+    }
+
+
+    IEnumerator ChangeButterflyTargetLocation()
+    {
+        while (gameObject.activeSelf)
+        {
+            //target.position = SetNewTargetLocation();
+            //Vector3 targetMoveDir = (target.position - butterflyModel.position).normalized;
+            //target.rotation *= Quaternion.FromToRotation(target.forward, targetMoveDir);
+            //initModelRot = butterflyModel.rotation;
+            SetNewTargetLocation();
+            yield return new WaitForSeconds(changeTargetLocRate.GetRndValue());
+        }
+    }
+
+}
