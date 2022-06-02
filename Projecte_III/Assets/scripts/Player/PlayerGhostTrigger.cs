60107408ef347e40ba048a167fdc56f192673e74
Jordi <jordi.herrera@enti.cat>
2022-06-02 09:38:00 +0200

    ghost time fixed

--- Projecte_III/Assets/scripts/Player/PlayerGhostTrigger.cs
+++ Projecte_III/Assets/scripts/Player/PlayerGhostTrigger.cs
@@ -8,6 +8,10 @@ public class PlayerGhostTrigger : MonoBehaviour
     Rigidbody playerRB;
     PlayerVehicleScript playerScript;
 
+    Vector3 currDir = Vector3.zero;
+
+    int playersColliding = 0;
+
     private void Start()
     {
         playerEvents = transform.parent.parent.GetComponent<VehicleTriggerAndCollisionEvents>();
@@ -19,25 +23,39 @@ public class PlayerGhostTrigger : MonoBehaviour
     {
         if (playerEvents.inmunity && other.gameObject.tag.Contains("Player"))
         {
-            if(playerEvents.TimerRespawn < 0.5f)
-            {
-                playerScript.dash = true;
-                float _speed = 10;
-                Vector3 dir = -(transform.position - other.transform.position);
-                dir = new Vector3(dir.x, 0.0f, 0.0f).normalized;
-                Vector3 dirLocal = transform.InverseTransformDirection(dir);
-                playerRB.velocity = transform.TransformDirection(dirLocal.x * _speed, dirLocal.y * _speed, transform.InverseTransformDirection(playerRB.velocity).z);
-            }
+            playersColliding++;
             playerEvents.StopGhost = false;
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (playerEvents.inmunity && other.gameObject.tag.Contains("Player"))
+        {
+            if (playerEvents.TimerRespawn < 0.5f)
+            {
+                playerScript.dash = true;
+                float _speed = 10;
+                currDir += -(transform.position - other.transform.position) * Time.deltaTime;
+                currDir = new Vector3(currDir.x, 0.0f, 0.0f).normalized;
+                Vector3 dirLocal = transform.InverseTransformDirection(currDir);
+                playerRB.velocity = transform.TransformDirection(dirLocal.x * _speed, dirLocal.y * _speed, transform.InverseTransformDirection(playerRB.velocity).z);
+            }
+        }
+    }
+
 
     private void OnTriggerExit(Collider other)
     {
         if (playerEvents.inmunity && other.gameObject.tag.Contains("Player"))
         {
-            playerScript.dash = false;
-            playerEvents.StopGhost = true;
+            playersColliding--;
+            if(playersColliding == 0 || playersColliding < 0)
+            {
+                playersColliding = 0;
+                playerScript.dash = false;
+                playerEvents.StopGhost = true;
+            }
         }
     }
 }
