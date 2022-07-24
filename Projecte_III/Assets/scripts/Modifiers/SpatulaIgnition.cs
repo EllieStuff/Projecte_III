f89a92b3799dfcc35f4255d683ff09306b7fe5a3
xavifm <60983549+xavifm@users.noreply.github.com>
2022-07-24 13:37:36 +0200

    created the first spatula system (new umbrella replacement)

---
+++ Projecte_III/Assets/scripts/Modifiers/SpatulaIgnition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatulaIgnition : MonoBehaviour
+{
+    [SerializeField] PlayersManager players;
+    [SerializeField] PlayerVehicleScript localPlayer;
+    const float DISTANCE = 2f;
+    const float JUMPSPEED = 2.5f;
+
+    public void ThrowPlayer()
+    {
+        if(players == null)
+        players = GameObject.Find("PlayersManager").GetComponent<PlayersManager>();
+
+        for (int i = 0; i < players.players.Length; i++)
+        {
+            Transform otherPlayer = players.GetPlayer(i);
+            if (i != localPlayer.playerNum && Vector3.Distance(transform.GetChild(0).position, otherPlayer.transform.position) <= DISTANCE)
+            {
+                otherPlayer.GetComponent<PlayerVehicleScript>().vehicleRB.velocity = new Vector3(0, JUMPSPEED * 8, 0);
+                break;
+            }
+        }
+    }
+}
