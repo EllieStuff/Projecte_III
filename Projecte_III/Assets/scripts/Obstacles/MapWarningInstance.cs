3f17e6f5baf342971b2d0850a5fc480692f37de0
xavifm <60983549+xavifm@users.noreply.github.com>
2022-09-06 22:02:17 +0200

    added effect sound to the warning system

--- Projecte_III/Assets/scripts/Obstacles/MapWarningInstance.cs
+++ Projecte_III/Assets/scripts/Obstacles/MapWarningInstance.cs
@@ -7,6 +7,11 @@ public class MapWarningInstance : MonoBehaviour
 {
     internal float time = 1;
 
+    private void Start()
+    {
+        GetComponent<AudioSource>().pitch = Random.Range(0.5f, 1);
+    }
+
     void Update()
     {
         if (time <= 0)
